8494d9d121919a3332f18492182f9de8df2813c3
Kyryl Andreiev <kiril.andreyev@gmail.com>
2021-11-18 14:19:14 +0200

    Code enchantments

--- main.py
+++ main.py
@@ -43,37 +43,6 @@ def load_list(cursor):
         res.append(x[0])
     return res
 
-async def ttdownoad(message):
-    url = message.text
-    dl_url = 'https://toolav.herokuapp.com/id/?video_id='
-
-    mobile_pattern = re.compile(r'(https?://[^\s]+tiktok.com/[^\s@]+)') # re pattern for tiktok links from mobile, e.x. "vm.tiktok.com/"
-    web_pattern = re.compile(r'(https?://www.tiktok.com/@[^\s]+/video/[0-9]+)') # re pattern for tiktok links from a web browser, e.x. "www.tiktok.com/@user"
-
-    if mobile_pattern.search(url):
-        r = rget(url).url # GET request to get the ID of the tiktok
-        if r != 'https://www.tiktok.com/':
-            tiktok_id = r.split('.html', 1)[0].split('/')[-1]
-        else:
-            return await message.answer('Недействительная ссылка!', parse_mode='HTML')
-
-    elif web_pattern.search(url):
-        tiktok_id = url.split('?', 1)[0].split('/')[-1]
-
-    else:
-        return await message.answer('Некоректная ссылка!')
-
-    msg = await message.answer('<code>Выполняеться запрос видео</code>', parse_mode='HTML')
-    r = rget(dl_url + tiktok_id)
-    text = r.json()
-    if 'status_code' in text:
-        await msg.edit_text('Недействительная ссылка!', parse_mode='HTML')
-    else:
-        await msg.edit_text('<code>Отправка видео</code>', parse_mode='HTML')
-        playAddr = text['item']['video']['playAddr']
-        await message.answer_video(playAddr[0])
-        await msg.delete()
-
 #Команда /start
 @dp.message_handler(commands=['start'])
 async def send_start(message: types.Message):
@@ -81,7 +50,13 @@ async def send_start(message: types.Message):
         cursor.execute(f'INSERT INTO Users VALUES ({message.chat.id}, {tCurrent()})')
         sqlite.commit()
         users.append(message.chat.id)
-    await message.answer('Вы запуситили <b>ttdownload</b>', parse_mode="HTML")
+    await message.answer('Вы запуситили бота <b>No Watermark TikTok</b>\nЭтот бот позволяет скачивать видео из тиктока <b><i>без водяного знака</i></b>.\n<b>Отправьте ссылку на видео чтобы начать</b>', parse_mode="HTML")
+
+@dp.message_handler(commands=["users", "len"])
+async def send_notify(message: types.Message):
+    if message.chat.id == admin_id:
+        lenusr = len(users)
+        await message.answer(f'Пользователей в боте: <b>{lenusr}</b>', parse_mode='HTML')
 
 @dp.message_handler(commands=["notify"])
 async def send_notify(message: types.Message):
@@ -90,7 +65,7 @@ async def send_notify(message: types.Message):
         await states.notify.set()
 
 @dp.message_handler(content_types=['video'], state=states.notify)
-async def notify_load(message: types.Message, state: FSMContext):
+async def notify_video(message: types.Message, state: FSMContext):
     msg = await message.answer('<code>Началась рассылка</code>', parse_mode='HTML')
     num = 0
     for x in users:
@@ -104,7 +79,7 @@ async def notify_load(message: types.Message, state: FSMContext):
     await state.finish()
 
 @dp.message_handler(content_types=['photo'], state=states.notify)
-async def notify_load(message: types.Message, state: FSMContext):
+async def notify_photo(message: types.Message, state: FSMContext):
     msg = await message.answer('<code>Началась рассылка</code>', parse_mode='HTML')
     num = 0
     for x in users:
@@ -118,7 +93,7 @@ async def notify_load(message: types.Message, state: FSMContext):
     await state.finish()
 
 @dp.message_handler(content_types=['animation'], state=states.notify)
-async def notify_load(message: types.Message, state: FSMContext):
+async def notify_gif(message: types.Message, state: FSMContext):
     msg = await message.answer('<code>Началась рассылка</code>', parse_mode='HTML')
     num = 0
     for x in users:
@@ -132,7 +107,7 @@ async def notify_load(message: types.Message, state: FSMContext):
     await state.finish()
 
 @dp.message_handler(content_types=['text'], state=states.notify)
-async def notify_load(message: types.Message, state: FSMContext):
+async def notify_text(message: types.Message, state: FSMContext):
     msg = await message.answer('<code>Началась рассылка</code>', parse_mode='HTML')
     num = 0
     for x in users:
@@ -146,7 +121,7 @@ async def notify_load(message: types.Message, state: FSMContext):
     await state.finish()
 
 @dp.message_handler(content_types=['document'], state=states.notify)
-async def notify_load(message: types.Message, state: FSMContext):
+async def notify_doc(message: types.Message, state: FSMContext):
     msg = await message.answer('<code>Началась рассылка</code>', parse_mode='HTML')
     num = 0
     for x in users:
@@ -161,12 +136,52 @@ async def notify_load(message: types.Message, state: FSMContext):
 
 @dp.message_handler()
 async def send_ttdown(message: types.Message):
-    await ttdownoad(message)
+    if message.chat.id not in active:
+        active.append(message.chat.id)
+        url = message.text
+        dl_url = 'https://toolav.herokuapp.com/id/?video_id='
+
+        mobile_pattern = re.compile(r'(https?://[^\s]+tiktok.com/[^\s@]+)') # re pattern for tiktok links from mobile, e.x. "vm.tiktok.com/"
+        web_pattern = re.compile(r'(https?://www.tiktok.com/@[^\s]+/video/[0-9]+)') # re pattern for tiktok links from a web browser, e.x. "www.tiktok.com/@user"
+
+        if mobile_pattern.search(url):
+            r = rget(url).url # GET request to get the ID of the tiktok
+            if r != 'https://www.tiktok.com/':
+                tiktok_id = r.split('.html', 1)[0].split('/')[-1]
+            else:
+                return await message.answer('Недействительная ссылка!', parse_mode='HTML')
+
+        elif web_pattern.search(url):
+            tiktok_id = url.split('?', 1)[0].split('/')[-1]
+
+        else:
+            return await message.answer('Некоректная ссылка!')
+
+        msg = await message.answer('<code>Выполняеться запрос видео</code>', parse_mode='HTML')
+        r = rget(dl_url + tiktok_id)
+        text = r.json()
+        if 'status_code' in text:
+            await msg.edit_text('Недействительная ссылка!', parse_mode='HTML')
+        else:
+            await msg.edit_text('<code>Отправка видео</code>', parse_mode='HTML')
+            playAddr = text['item']['video']['playAddr']
+            await message.reply_video(playAddr[0])
+            await msg.delete()
+            active.remove(message.chat.id)
+    else:
+        await message.reply('Вы еще не скачали прошлое видео')
 
 if __name__ == "__main__":
+
+    #Подключение sqlite
     sqlite = sqlite_init()
+    #sqlite.row_factory = lambda cursor, row: row[0]
     cursor = sqlite.cursor()
+
+    #Инициализация базы даних
+    active = list()
     users = load_list(cursor)
+
     executor.start_polling(dp, skip_updates=True)
     sqlite.close()
     print("Соединение с SQLite закрыто")
