e97f5e889f6025464305c35686910e379d57ba79
Kyryl Andreiev <kiril.andreyev@gmail.com>
2021-11-20 20:55:17 +0200

    chat id in log

--- main.py
+++ main.py
@@ -258,7 +258,7 @@ async def send_ttdown(message: types.Message):
             await message.reply_video(playAddr[0], caption=podp_text, parse_mode='markdown')
             await msg.delete()
         active.remove(message.chat.id)
-        print(url)
+        print(f'{message.chat.id}: {url}')
     else:
         await message.reply('Вы еще не скачали прошлое видео')
 
