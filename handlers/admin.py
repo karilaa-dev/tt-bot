88877335ed3d2f041495e6062d2a0204839e8ccb
Kyryl Andreiev <karilaa@karilaa.dev>
2023-09-10 22:33:42 -0700

    Remove TrueCheck

--- handlers/admin.py
+++ handlers/admin.py
@@ -1,8 +1,6 @@
-from asyncio import sleep
 from io import BytesIO
 
 from aiogram import types
-from truechecker import TrueChecker
 
 from data.config import admin_ids, second_ids, bot_token
 from data.loader import bot, cursor, dp
@@ -19,31 +17,6 @@ async def send_hi(message: types.Message):
             await message.answer('done')
         except:
             await message.answer('ops')
-
-
-@dp.message_handler(commands=['truecheck'])
-async def truecheck(message: types.Message):
-    if message["from"]["id"] in admin_ids or message["from"]["id"] in second_ids:
-        users = cursor.execute('SELECT id FROM users').fetchall()
-        with open('users.txt', 'w') as f:
-            for x in users:
-                f.write(str(x[0]) + '\n')
-        checker = TrueChecker(bot_token)
-        job = await checker.check_profile("users.txt")
-        msg = await message.answer('Проверка запущена')
-        progress = 0
-        while int(progress) != 100:
-            job = await checker.get_job_status(job.id)
-            if job.progress != progress:
-                await msg.edit_text(f'Проверено <b>{job.progress}%</b>')
-            progress = job.progress
-            await sleep(3)
-        username = (await dp.bot.me)['username']
-        profile = await checker.get_profile(username)
-        res = f'Users\n - alive: {profile.users.active}\n - stopped: {profile.users.stopped}\n - deleted: {profile.users.deleted}\n - not found: {profile.users.not_found}'
-        await msg.delete()
-        await message.answer(res)
-        await checker.close()
 
 
 @dp.message_handler(commands=["export"], state='*')
