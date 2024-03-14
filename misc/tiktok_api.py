6b74ffd78328de14125ed5984a0b09fc0269bf15
Kyryl Andreiev <karilaa@karilaa.dev>
2024-03-14 14:07:19 -0700

    Fix tiktop api link

--- misc/tiktok_api.py
+++ misc/tiktok_api.py
@@ -7,7 +7,7 @@ from data.loader import bot
 
 class ttapi:
     def __init__(self):
-        self.url = 'https://api16-normal-c-useast1a.tiktokv.com/aweme/v1/feed/'
+        self.url = 'https://api22-normal-c-useast2a.tiktokv.com/aweme/v1/feed/'
         self.params = {"pull_type": 4, "interest_list": "{\"special_type\":0,\"recommend_group\":1}"}
         self.headers = {
             'User-Agent': 'com.ss.android.ugc.trill/494+Mozilla/5.0+(Linux;+Android+12;+2112123G+Build/SKQ1.211006.001;+wv)+AppleWebKit/537.36+(KHTML,+like+Gecko)+Version/4.0+Chrome/107.0.5304.105+Mobile+Safari/537.36'
